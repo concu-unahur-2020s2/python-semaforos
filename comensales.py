859f574125592d7d95879d840924eb1ec5f5cbeb
Martin Acuña <msguser@gmail.com>
2020-08-29 02:58:03 -0300

    Hice modificaciones en comensales luego de la clase

--- comensales.py
+++ comensales.py
@@ -3,9 +3,12 @@ import time
 import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+#luego de la clase repense el ejercicio e hice algunas modificaciones.
 
-#defino el semaforo
-semaforoDeComida = threading.Semaphore(3)
+#el despertador comienza en cero para que el cocinero espere a que el comensal lo llame.
+#lo mismo con el semaforo de comida.
+semaforoDespertador = threading.Semaphore(0)
+semaforoDeComida = threading.Semaphore(0)
 
 class Cocinero(threading.Thread):
   def __init__(self):
@@ -15,10 +18,15 @@ class Cocinero(threading.Thread):
   def run(self):
     global platosDisponibles
     while (True):
-      #tomo un semaforo
-      semaforoDeComida.acquire()
+      #en la primera vuelta del while no corre porque inicializó en cero.
+      #tomo el semaforo despertador asi el cocinero no cocina hasta que el comensal lo llame.
+      semaforoDespertador.acquire()
       logging.info('Reponiendo los platos...')
       platosDisponibles = 3
+      #una vez que esten los platos libero el semaforo de comida.
+      semaforoDeComida.release()
+
+
 
 class Comensal(threading.Thread):
   def __init__(self, numero):
@@ -27,10 +35,19 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+
+    #si la cantidad de platos disponible es cero bloqueo el semaforo de comida
+    if platosDisponibles == 0:
+      semaforoDeComida.acquire()
+
     platosDisponibles -= 1
-    #libero el semaforo
-    semaforoDeComida.release()
+
+    #cuando no quedan platos despierto al cocinero
+    if platosDisponibles == 0:
+      semaforoDespertador.release()
+
     logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+
 
 platosDisponibles = 3
 
@@ -38,4 +55,3 @@ Cocinero().start()
 
 for i in range(5):
   Comensal(i).start()
-
