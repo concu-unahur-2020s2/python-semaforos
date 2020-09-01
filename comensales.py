ae8435222acbe8b072041a8fe257812749ca5f7a
adrian <agyaniri@gmail.com>
2020-08-31 21:47:05 -0300

    comensales terminado

--- comensales.py
+++ comensales.py
@@ -4,6 +4,10 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+semaphone = threading.Semaphore(2)
+semaphoneCocinero = threading.Semaphore(0)
+
+
 class Cocinero(threading.Thread):
   def __init__(self):
     super().__init__()
@@ -11,9 +15,13 @@ class Cocinero(threading.Thread):
 
   def run(self):
     global platosDisponibles
+
+    semaphone.acquire()
     while (True):
+      semaphone.acquire()
       logging.info('Reponiendo los platos...')
       platosDisponibles = 3
+    
 
 class Comensal(threading.Thread):
   def __init__(self, numero):
@@ -22,8 +30,11 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+
     platosDisponibles -= 1
     logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+    semaphoneCocinero.release()
+
 
 platosDisponibles = 3
 
