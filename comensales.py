1a09f9071abbce8cb418afda334f22324db4f143
Fabiola <fabiolaelena98@gmail.com>
2020-09-03 07:43:40 -0300

    Modifique comensales

--- comensales.py
+++ comensales.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 semaphoreComensal = threading.Semaphore(3)
+semaphoreComida = threading.Semaphore(0)
 
 class Cocinero(threading.Thread):
   def __init__(self):
@@ -15,8 +16,11 @@ class Cocinero(threading.Thread):
     global platosDisponibles
     while (True):
       semaphoreComensal.acquire()
+      #El semaforo comida hace que el cocinero no cocine hasta que el comensal lo llame.
+      semaphoreComida.acquire()
       logging.info('Reponiendo los platos...')
       platosDisponibles = 3
+      semaphoreComensal.release()
       
       
 class Comensal(threading.Thread):
@@ -26,8 +30,15 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+    #Si los platos disponibles son 0 se bloquea el semaforo comensal.
+    if platosDisponibles == 0:
+      semaphoreComensal.acquire()
     platosDisponibles -= 1
     semaphoreComensal.release()
+    #Cuando no halla mas platos despierto al cocinero
+    if platosDisponibles == 0:
+      semaphoreComida.release()
+    
 
     logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
 
@@ -37,6 +48,6 @@ Cocinero().start()
 
 for i in range(5):
   Comensal(i).start()
-  if platosDisponibles == 0:
-    Cocinero().start()
+ 
+  Cocinero().start()
 
